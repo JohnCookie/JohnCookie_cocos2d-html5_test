4f298ee0f99ad1cb9ff572466ea0987b70ae843a
JohnCookie <cookiejj2010@gmail.com>
2013-05-03 16:13:09 +0800

    Make the sprite walk automatically
    The sprite walk itself and keyboard control the direct( left or right )

--- Cocos2d-html5-v2.1.1/MyFirstApp/MyFifthApp.js
+++ Cocos2d-html5-v2.1.1/MyFirstApp/MyFifthApp.js
@@ -4,6 +4,7 @@ var MyFifthApp = cc.Layer.extend({
 	spriteFrameIndex: 0,
 	layerColor: null,
 	spriteX: 300,
+	walkDirect:0, //0 walkleft 1 walkright
 	init: function(){
 		this._super();
 		this.layerColor = cc.LayerColor.create(new cc.Color4B(0,0,0,255));
@@ -23,7 +24,12 @@ var MyFifthApp = cc.Layer.extend({
 
 		this.setKeyboardEnabled(true);
 
+		this.schedule(this.walk);
+
 		cc.log(this.sprite.getContentSize());
+		cc.log("--- In Layer init ---");
+		cc.log(this.layerColor);
+		cc.log(this.sprite);
 
 		return this;
 	},
@@ -31,6 +37,7 @@ var MyFifthApp = cc.Layer.extend({
 
 	},
 	onKeyDown: function(e){
+		/*
 		if(e==cc.KEY.left || e==cc.KEY.right){
 			var prevPrefix =  this.spriteFrameNamePrefix;
 			if(e==cc.KEY.left){
@@ -73,6 +80,66 @@ var MyFifthApp = cc.Layer.extend({
 			this.layerColor.addChild(this.sprite);
 			this.spriteFrameIndex++;
 		}
+		*/
+		if(e==cc.KEY.left){
+			this.walkDirect=0;
+			cc.log("Turn left");
+			cc.log(this.walkDirect);
+		}
+		if(e==cc.KEY.right){
+			cc.log("Turn Right");
+			this.walkDirect=1;
+			cc.log(this.walkDirect);
+		}
+	},
+	walk: function(dt){
+		var prevPrefix =  this.spriteFrameNamePrefix;
+		if(this.walkDirect==0){
+			this.spriteFrameNamePrefix="Walk_left";
+			this.spriteX-=2;
+		}else{
+			this.spriteFrameNamePrefix="Walk_right";
+			this.spriteX+=2;
+		}
+
+		if(this.spriteX<0){
+			this.spriteX=0;
+			this.walkDirect=1;
+		}
+		if(this.spriteX>600){
+			this.spriteX=600;
+			this.walkDirect=0;
+		}
+
+		if(prevPrefix!== this.spriteFrameNamePrefix){
+			this.spriteFrameIndex=0;
+		}
+
+		if(this.spriteFrameIndex>18){
+			this.spriteFrameIndex=0;
+		}
+
+		var indexAsString;
+		if(this.spriteFrameIndex<10){
+			indexAsString="0"+this.spriteFrameIndex.toString();
+		}else{
+			indexAsString=this.spriteFrameIndex.toString();
+		}
+
+		// cc.log("--- In Layer update walk function ---");
+		// cc.log(this);
+		// cc.log(this.layerColor);
+		// cc.log(this.sprite);
+
+		this.layerColor.removeChild(this.sprite);
+		this.sprite=cc.Sprite.createWithSpriteFrameName(this.spriteFrameNamePrefix+indexAsString+".png");
+
+		this.sprite.setPosition(new cc.Point(this.spriteX,300));
+		// this.sprite.setAnchorPoint(cc.PointMake(0,0.5));
+		// this.sprite.setScale(3);
+
+		this.layerColor.addChild(this.sprite);
+		this.spriteFrameIndex++;
 	}
 });
 
