6a25d07fb5145b27f1addbe0ce869f3fcc0a4ac1
JohnCookie <cookiejj2010@gmail.com>
2013-05-20 16:00:41 +0800

    Now the collision happened both on x,y axis
    The collision happened both on x-axis and y-axis.

--- Cocos2d-html5-v2.1.1/MyFirstApp/SimpleCollision.js
+++ Cocos2d-html5-v2.1.1/MyFirstApp/SimpleCollision.js
@@ -1,7 +1,7 @@
 var SimpleCollisionLayer = cc.Layer.extend({
 	ball1:null,
 	ball2:null,
-	bounceMinus:-0.5,
+	bounceMinus:-1,
 	init:function(){
 		this._super();
 		var size = cc.Director.getInstance().getWinSize();
@@ -12,8 +12,10 @@ var SimpleCollisionLayer = cc.Layer.extend({
 		ball1=new SimpleBall(50, size.height/2, 30, 10, new cc.Color4B(255,0,0,255));
 		ball2=new SimpleBall(550, size.height/2, 40, 15, new cc.Color4B(255,0,100,255));
 		//init the speed of the ball
-		ball1.vx=5;
-		ball2.vx=-5;
+		ball1.vx=5*(Math.random()*2-1);
+		ball1.vy=5*(Math.random()*2-1);
+		ball2.vx=5*(Math.random()*2-1);
+		ball2.vy=5*(Math.random()*2-1);
 
 		layerColor.addChild(ball1);
 		layerColor.addChild(ball2);
@@ -25,11 +27,20 @@ var SimpleCollisionLayer = cc.Layer.extend({
 	update:function(){
 		//Update the ball position
 		ball1.x+=ball1.vx;
+		ball1.y+=ball1.vy;
 		ball2.x+=ball2.vx;
+		ball2.y+=ball2.vy;
+
 		//Calculate the distance between 2 ball
-		var dist=ball1.x-ball2.x;
+		// var dist=ball1.x-ball2.x; //on x-axis
+		var dx=ball2.x-ball1.x;
+		var dy=ball2.y-ball1.y;
+		var dist=Math.sqrt(dx*dx+dy*dy);
+
 		//if 2 balls crashed
 		if(Math.abs(dist)<ball1.radius+ball2.radius){
+			// on x-axis
+			/*
 			// v0final=((m0-m1)v0+2m1v1)/(m0+m1)
 			// v1final=v0final+(v0-v1)
 			var vdx=ball1.vx-ball2.vx;
@@ -43,8 +54,72 @@ var SimpleCollisionLayer = cc.Layer.extend({
 
 			ball1.x+=ball1.vx;
 			ball2.x+=ball2.vx;
+			*/
+
+			// on both x-axis and y-axis
+			var angle=Math.atan2(dy,dx);
+			var cos=Math.cos(angle);
+			var sin=Math.sin(angle);
+
+			// rotate by the center of ball1
+			var x1=0;
+			var y1=0;
+
+			var x2=dx*cos+dy*sin;
+			var y2=dy*cos-dx*sin;
+
+			//the speed after rotate
+			var vx1=ball1.vx*cos+ball1.vy*sin;
+			var vy1=ball1.vy*cos-ball1.vx*sin;
+			var vx2=ball2.vx*cos+ball2.vy*sin;
+			var vy2=ball2.vy*cos-ball2.vx*sin;
+
+			// v0final=((m0-m1)v0+2m1v1)/(m0+m1)
+			// v1final=v0final+(v0-v1)
+			var vdx=vx1-vx2;
+			var vx1final=((ball1.mass-ball2.mass)*vx1+2*ball2.mass*vx2)/(ball1.mass+ball2.mass);
+			var vx2final=vx1final+vdx;
+
+			// the new position
+			// x1+=vx1final;
+			// x2+=vx2final;
+			var sumRadius=ball1.radius+ball2.radius;
+			var overlap=sumRadius-Math.abs(x1-x2);
+			
+			var radio1=ball1.radius/sumRadius;
+			var radio2=ball2.radius/sumRadius;
+
+			if(overlap>0){
+				if(x1>x2){
+					x1+=overlap*radio1;
+					x2-=overlap*radio2;
+				}else{
+					x1-=overlap*radio1;
+					x2+=overlap*radio2;
+				}
+			}
+
+			//rotate back
+			var x1final=x1*cos-y1*sin;
+			var y1final=y1*cos+x1*sin;
+			var x2final=x2*cos-y2*sin;
+			var y2final=y2*cos+x2*sin;
+
+			ball2.x=ball1.x+x2final;
+			ball2.y=ball1.y+y2final;
+			ball1.x+=x1final;
+			ball1.y+=y1final;
+			
+			//final speed
+			ball1.vx=vx1final*cos-vy1*sin;
+			ball1.vy=vy1*cos+vx1final*sin;
+			ball2.vx=vx2final*cos-vy2*sin;
+			ball2.vy=vy2*cos+vx2final*sin;
+
 		}
 		//bounce when touch the border
+		// on only x-axis
+		/*
 		if(ball1.x<=0+ball1.radius || ball1.x>=600-ball1.radius){
 			ball1.x-=ball1.vx;
 			ball1.vx*=this.bounceMinus;
@@ -53,6 +128,9 @@ var SimpleCollisionLayer = cc.Layer.extend({
 			ball2.x-=ball2.vx;
 			ball2.vx*=this.bounceMinus;
 		}
+		*/
+		this.checkBounce(ball1);
+		this.checkBounce(ball2);
 
 		if (Math.abs(ball1.vx)<=1 && Math.abs(ball2.vx)<=1){
 			cc.log("End");
@@ -62,6 +140,26 @@ var SimpleCollisionLayer = cc.Layer.extend({
 			cc.log(ball2);
 			this.unschedule(this.update);
         }
+	},
+	checkBounce: function(ball){
+		if(ball.x<ball.radius){
+			//left
+			ball.x=ball.radius;
+			ball.vx*=this.bounceMinus;
+		}else if(ball.x>600-ball.radius){
+			//right
+			ball.x=600-ball.radius;
+			ball.vx*=this.bounceMinus;
+		}
+		if(ball.y<ball.radius){
+			//bottom
+			ball.y=ball.radius;
+			ball.vy*=this.bounceMinus;
+		}else if(ball.y>600-ball.radius){
+			//top
+			ball.y=600-ball.radius;
+			ball.vy*=this.bounceMinus;
+		}
 	}
 });
 
